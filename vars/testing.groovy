4209f151ade16f32f9e3443f1ac23c1c64f9f1f8
amithapa <amitt@unicourt.com>
2018-10-03 17:08:14 +0530

    Shared Libraries in jenkins

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,4 +1,5 @@
 def call(String git_repository, String git_username='amithapa', String git_branch='staging', String pipeline_json_path='pipeline.json') {
+    def pipeline_map_data
     stage('checkout') {
         node('master') {
             // Checks Out to Specific Branch
@@ -8,9 +9,17 @@ def call(String git_repository, String git_username='amithapa', String git_branc
             data = readFile pipeline_json_path
             // Convert to map
             def jsonSlurper = new groovy.json.JsonSlurper()
-            def pipeline_map_data = jsonSlurper.parseText(data)
+            pipeline_map_data = jsonSlurper.parseText(data)
 
             echo data
         }
     }
+
+    stage('Build and Test') {
+        node('master') {
+            echo pipeline_map_data.application_name
+        }
+    }
+
+
 }
