ad28cd52e691398603e7d7fd69028fb219ebafc5
amithapa <amitt@unicourt.com>
2018-10-03 16:42:59 +0530

    Shared Libraries in jenkins

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,9 +1,9 @@
-def call() {
+def call(Map data) {
     stage('checkout') {
         node('master') {
             // Checks Out to Specific Branch
             git branch: 'staging', url: 'git@github.com:amithapa/development_docker.git'
-
+            echo data
         }
     }
 }
